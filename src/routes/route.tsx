a422c89c2a47ce66d5f6d6bb896225bd608a7be9
Gui Farias <gui.farias@outlook.com.br>
2025-07-23 14:11:52 -0300

    add 404

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/index';
 import Login from '../pages/login';
 import Register from '../pages/register';
@@ -12,6 +12,7 @@ export default function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
